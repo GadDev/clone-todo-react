81d689ae4a21f5d1925b30476c9526d6895b6ad0
Alex Gadaix <alex.gadaix@gmail.com>
2021-06-22 05:39:47 +0100

    building header comp

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import { FaPizzaSLice } from 'react-icons/fa';
 export const Header = () => {
 	return (
-		<div>
-			header
-		</div>
-	)
-}
+		<header className='header' data-testid='header'>
+			<nav>
+				<div className='logo'>
+					<img src='/images/logo.png' alt='' />
+				</div>
+				<div className='settings'>
+					<ul>
+						<li>+</li>
+						<li>
+							<FaPizzaSLice />
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
+};
